ef10a10e43c53a95d7295bb713489fe4a77b9918
yammybajwa <YAMMYBAJWA09@GMAIL.COM>
2022-02-03 12:19:35 +0500

    dynamic content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React ,{component} from "react";
+import React, { component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
-    this.state ={
-      String:"My Name is zain"
+    this.state = {
+      characher :[
+        {
+          name: "Yammy",
+          id:"asd4"
+        },
+        {
+          name: "Franky",
+          id:"agd5"
+
+        },
+        {
+          name: "John",
+          id:"adt8"
+
+        }
+      ]
+
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.String}
-
-           </p>
-          <button onClick={()=> this.setState({String:"Yammy Is Here"})}>Change Text</button>
-        </header>
+        {this.state.characher.map(characher => <h1 key={characher.id}>{characher.name}</h1>)}
       </div>
     );
   }
