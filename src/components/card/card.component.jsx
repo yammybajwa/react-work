bad49edc9639a271c255b43311c8eae9372ec276
yammybajwa <YAMMYBAJWA09@GMAIL.COM>
2022-02-06 22:34:16 +0500

    control width and size

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ export const Card = props =>(
     <div className="card-container">
         <img
          alt="characher"
-         src={`https://robohash.org/${props.characher.id}?set=set2&size=180*180`}  />
+         src={`https://robohash.org/${props.characher.id}?set=set2`} width={180} height={180} />
         <h2>{props.characher.name}</h2>
         <p>{props.characher.email}</p>
     </div>
